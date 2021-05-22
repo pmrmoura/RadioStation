4f15452c9a3cc396c2d63634398977080ad07bc3
Pedro da Matta <pmrm@cin.ufpe.br>
2021-05-22 02:12:02 -0300

    Setup(project): Initial setup

---
+++ radiostation/src/components/Radio/Radio.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Radio() {
+    return (
+        <div>
+            oi
+        </div>
+    )
+}
+
+export default Radio;
---
+++ radiostation/src/components/RadioContainer/RadioContainer.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import styles from './RadioContainer.modules.css';
+
+function RadioContainer() {
+    return (
+        <div className={styles.error}>
+            oi
+        </div>
+    )
+}
+
+export default RadioContainer;
